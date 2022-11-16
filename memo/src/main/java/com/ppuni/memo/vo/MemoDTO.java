15fc9de3e4da9726d8ab83e55374bc7821781afa
ppunikim <yd62322@naver.com>
2022-11-16 21:07:52 +0900

    메모 시작

---
+++ memo/src/main/java/com/ppuni/memo/vo/MemoDTO.java
@@ -0,0 +1,9 @@
+package com.ppuni.memo.vo;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class MemoDTO {
+
+}
---
+++ memo/src/main/resources/static/memo/first.jsp
@@ -0,0 +1,5 @@
+<script type="application/javascript">
+
+</script>
+
+<div>첫 번째 memo</div>
